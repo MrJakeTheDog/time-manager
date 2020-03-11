bed0a96271498d5d0146859649f07108a2453cb1
Mikhail <ekazamen_na_5@ukr.net>
2020-03-11 19:55:25 +0200

    add firebase conection to database

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://time-manager-6d8bd.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("server/saving-data/fireblog");
+
+var usersRef = ref.child("users");
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});
+
 let express = require('express'),
 app = express(),
 // bodyParser = require('body-parser'),
